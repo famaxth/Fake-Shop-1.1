eaffb614e17006d2ecfbb04a2c4ca249b1a04456
Kalir00tKali <horrorclownDM@gmail.com>
2020-12-07 18:55:27 +0300

    SALE

---
+++ config.py
@@ -0,0 +1,7 @@
+name = "text" #Название Магазина
+token = "text" #Токен бота
+admin_id = 123456789 #ID администратора
+operator = "@admin" #Логин оператора
+qiwi = "+7999999999" #Qiwi номер
+token_qiwi = "text" #Указываем Qiwi токен
+bitcoin = "11111111111111111" #Указываем Bitcoin кошелек
